626cf43aaf07ce1c3e57a3d4fd51c75b4b54f89e
Qinastha <qinastha@gmail.com>
2025-03-13 01:18:54 +0200

    Minor UI fix

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -62,7 +62,7 @@ export default function DashboardPage() {
                     variant="default"
                   >
                     {item.underDev ? (
-                      <span className="whitespace-normal break-words text-center">
+                      <span className="whitespace-normal break-words text-center !text-foreground">
                         {tDash('underDev')} {tDash(item.title).toLowerCase()}
                       </span>
                     ) : (
